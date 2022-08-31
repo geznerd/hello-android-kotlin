d7bfcba0bf388e8b36b21caab152c460e23786ce
mazmis <mazmis@hotmail.com>
2022-08-31 09:40:19 +0700

    menambahkan hewan pertama ke aplikasi

--- app/src/main/java/com/azmi/helloworld/MainActivity.kt
+++ app/src/main/java/com/azmi/helloworld/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.azmi.helloworld
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    //
+
+    private lateinit var imageView: ImageView
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,5 +30,15 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        imageView =findViewById(R.id.imageView)
+        textView = findViewById(R.id.textView)
+
+        showData(data[3])
+
+    }
+
+    private  fun showData(hewan: Hewan) {
+        imageView.setImageResource(hewan.gambarResId)
+        textView.text = hewan.nama
     }
 }
