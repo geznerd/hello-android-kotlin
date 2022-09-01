4d36ebbb05619fd5d8b159ae6cff0ddeba1750f7
mazmis <mazmis@hotmail.com>
2022-09-01 09:51:52 +0700

    menambahkan data hewan

--- app/src/main/java/com/azmi/helloworld/MainActivity.kt
+++ app/src/main/java/com/azmi/helloworld/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.azmi.helloworld
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -14,14 +15,26 @@ class MainActivity : AppCompatActivity() {
         // data class Hewan -> ArrayList
         //========================
 
-
-        val data = ArrayList<Hewan>().apply {
-            add(Hewan("Ayam", R.drawable.ayam))
-            add(Hewan("Bebek", R.drawable.bebek))
-            add(Hewan("Domba", R.drawable.domba))
-            add(Hewan("Kambing", R.drawable.kambing))
-            add(Hewan("Sapi", R.drawable.sapi))
-        }
+        Log.d("MainActivity", "Jumlah data: ${getData().size}")
 
     }
+
+    private fun getData(): List<Hewan>{
+        return listOf(
+           Hewan("Ayam","Gallus gallu", R.drawable.ayam),
+           Hewan("Angsa","Cygnus olor", R.drawable.angsa),
+            Hewan("Bebek","Cairina moschata", R.drawable.bebek),
+            Hewan("Domba","Ovis ammo", R.drawable.domba),
+            Hewan("Kalkun","Meleagris gallopavo", R.drawable.kalkun),
+            Hewan("Kambing","Capricornis sumatrensis", R.drawable.kambing),
+            Hewan("Kelinci","Oryctolagus cuniculus", R.drawable.kelinci),
+            Hewan("Kerbau","Bubalus bubalis", R.drawable.kerbau),
+            Hewan("Kuda","Equus caballus", R.drawable.kuda),
+            Hewan("sapi","Bos taurus", R.drawable.sapi),
+
+        )
+
+    }
+
+
 }
