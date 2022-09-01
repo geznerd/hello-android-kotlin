e71c0b06b46cd6f9dbd0e6cb3a59be052c554dde
mazmis <mazmis@hotmail.com>
2022-09-01 14:23:01 +0700

    mengambil data dari internet

--- app/src/main/java/com/azmi/helloworld/MainViewModel.kt
+++ app/src/main/java/com/azmi/helloworld/MainViewModel.kt
@@ -1,31 +1,49 @@
 package com.azmi.helloworld
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 
     private val data = MutableLiveData<List<Hewan>>()
 
     init {
         data.value = initData()
+        retrieveDataFromServer()
+    }
+
+    private fun retrieveDataFromServer() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val result = HewanApi.service.getHewan()
+                Log.d("MainViewModel", result)
+            } catch (e: Exception) {
+                Log.d("MainViewModel", "Gagal: ${e.message}")
+            }
+        }
     }
 
     fun getData(): LiveData<List<Hewan>> = data
 
-    private fun initData(): List<Hewan>{
+    private fun initData(): List<Hewan> {
         return listOf(
-            Hewan("Ayam","Gallus gallu", R.drawable.ayam),
-            Hewan("Angsa","Cygnus olor", R.drawable.angsa),
-            Hewan("Bebek","Cairina moschata", R.drawable.bebek),
-            Hewan("Domba","Ovis ammo", R.drawable.domba),
-            Hewan("Kalkun","Meleagris gallopavo", R.drawable.kalkun),
-            Hewan("Kambing","Capricornis sumatrensis", R.drawable.kambing),
-            Hewan("Kelinci","Oryctolagus cuniculus", R.drawable.kelinci),
-            Hewan("Kerbau","Bubalus bubalis", R.drawable.kerbau),
-            Hewan("Kuda","Equus caballus", R.drawable.kuda),
-            Hewan("sapi","Bos taurus", R.drawable.sapi),
+            Hewan("Ayam", "Gallus gallu", R.drawable.ayam),
+            Hewan("Angsa", "Cygnus olor", R.drawable.angsa),
+            Hewan("Bebek", "Cairina moschata", R.drawable.bebek),
+            Hewan("Domba", "Ovis ammo", R.drawable.domba),
+            Hewan("Kalkun", "Meleagris gallopavo", R.drawable.kalkun),
+            Hewan("Kambing", "Capricornis sumatrensis", R.drawable.kambing),
+            Hewan("Kelinci", "Oryctolagus cuniculus", R.drawable.kelinci),
+            Hewan("Kerbau", "Bubalus bubalis", R.drawable.kerbau),
+            Hewan("Kuda", "Equus caballus", R.drawable.kuda),
+            Hewan("sapi", "Bos taurus", R.drawable.sapi),
 
             )
 
