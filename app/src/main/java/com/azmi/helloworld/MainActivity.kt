436efb86a52288171d7e4971aa5eebb74bd92bcd
mazmis <mazmis@hotmail.com>
2022-09-01 09:21:33 +0700

    membuat layout untuk list

--- app/src/main/java/com/azmi/helloworld/MainActivity.kt
+++ app/src/main/java/com/azmi/helloworld/MainActivity.kt
@@ -1,19 +1,10 @@
 package com.azmi.helloworld
 
-import android.media.Image
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
-    //
-
-    private lateinit var imageView: ImageView
-    private lateinit var textView: TextView
-
-    private  var index: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,27 +23,5 @@ class MainActivity : AppCompatActivity() {
             add(Hewan("Sapi", R.drawable.sapi))
         }
 
-        // val button
-
-        val button : Button = findViewById(R.id.button)
-
-        button.setOnClickListener {
-            index = (index + 1)  % data.size
-            showData(data[index])
-
-            //if(index == 5) index = 0
-        }
-
-
-        imageView =findViewById(R.id.imageView)
-        textView = findViewById(R.id.textView)
-
-        showData(data[0])
-
-    }
-
-    private  fun showData(hewan: Hewan) {
-        imageView.setImageResource(hewan.gambarResId)
-        textView.text = hewan.nama
     }
 }
