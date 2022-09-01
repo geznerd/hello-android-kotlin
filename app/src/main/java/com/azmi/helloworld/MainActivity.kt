b3b548f7f28e50c097695a3faa75243dcaed0061
mazmis <mazmis@hotmail.com>
2022-09-01 09:54:54 +0700

    menambahkan viewBinding

--- app/src/main/java/com/azmi/helloworld/MainActivity.kt
+++ app/src/main/java/com/azmi/helloworld/MainActivity.kt
@@ -3,13 +3,18 @@ package com.azmi.helloworld
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.azmi.helloworld.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //========================
         // data class Hewan -> ArrayList
